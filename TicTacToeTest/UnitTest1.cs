740a6d1197f43f660426d2a866e80e5baf4ff3a5
VoonaSindhura <voonasindhu67@gmail.com>
2024-02-23 11:54:41 +0000

    added couple of tet cases

--- TicTacToeTest/UnitTest1.cs
+++ TicTacToeTest/UnitTest1.cs
@@ -26,13 +26,23 @@ namespace TicTacToeTest
         }
 
         [Test]
-        public void Test_DiagonalWin()
+        public void Test_DiagonalWin_Player1()
         {
             string[,] board = new string[3, 3] {
             { "*", "*", "X" },
             { "*", "X", "*" },
             { "X", "*", "*" } };
             Assert.IsTrue(Program.CheckforWinner(board, 1));
+        }
+
+        [Test]
+        public void Test_DiagonalWin_Player2()
+        {
+            string[,] board = new string[3, 3] {
+            { "O", "*", "*" },
+            { "*", "O", "*" },
+            { "*", "*", "O" } };
+            Assert.IsTrue(Program.CheckforWinner(board, 2));
         }
 
         [Test]
@@ -59,7 +69,7 @@ namespace TicTacToeTest
         public void Test_GetCoordinates_ValidInput()
         {
             string input = "1 2";
-            Console.SetIn(new System.IO.StringReader(input)); // Set the console input to simulate user input
+            Console.SetIn(new StringReader(input)); // Set the console input to simulate user input
             Tuple<int, int> expected = Tuple.Create(1, 2);
             Assert.That(Program.GetCoordinates(), Is.EqualTo(expected));
         }
@@ -68,7 +78,7 @@ namespace TicTacToeTest
         public void Test_GetCoordinates_InValidInput()
         {
             string input = "3 1";
-            Console.SetIn(new System.IO.StringReader(input)); // Set the console input to simulate user input
+            Console.SetIn(new StringReader(input)); // Set the console input to simulate user input
             string expectedOutput = "Invalid input. Please enter two numbers between 0 and 2 separated by a space.\r\n";
 
             using (StringWriter sw = new StringWriter())
@@ -80,6 +90,43 @@ namespace TicTacToeTest
 
                 // Check the console output for the expected error message
                 Assert.That(sw.ToString().Trim(), Is.EqualTo(expectedOutput.Trim()));
+            }
+        }
+
+        [Test]
+        public void Test_OccupiedCell()
+        {
+            string[,] board = new string[3, 3] {
+        { "O", "X", "O" },
+        { "*", "*", "*" },
+        { "*", "*", "*" } };
+
+            string input = "0 0"; // Simulate user input
+
+            // Redirect Console input
+            using (StringReader sr = new StringReader(input))
+            {
+                Console.SetIn(sr);
+
+                // Redirect Console output
+                using (StringWriter sw = new StringWriter())
+                {
+                    Console.SetOut(sw);
+
+                    // Act and Assert
+                    Assert.Throws<NullReferenceException>(() => Program.PlayGame(board));
+
+                    // Check the console output for the expected error message
+                    string actualOutput = sw.ToString();
+                    string expectedOccupiedErrorMessage = "This cell is already occupied. Please choose another one.";
+
+                    // Extract the relevant part of the actual output
+                    int startIndex = actualOutput.IndexOf(expectedOccupiedErrorMessage);
+                    string actualOccupiedErrorMessage = actualOutput.Substring(startIndex, expectedOccupiedErrorMessage.Length);
+
+                    // Assert that the extracted part matches the expected part
+                    Assert.That(actualOccupiedErrorMessage, Is.EqualTo(expectedOccupiedErrorMessage));
+                }
             }
         }
     }
